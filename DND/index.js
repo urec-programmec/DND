155da204d280a9f37ba0230629d208fdf5d14f8c
svyat935 <svyat935@mail.ru>
2021-02-21 17:49:13 +0300

    Repull

--- DND/index.js
+++ DND/index.js
@@ -1,7 +1,35 @@
 $(document).ready(function () {
+    window.defaultWidth = window.innerWidth;
+    window.onresize = function () {
+
+        let x = (window.defaultWidth / window.innerWidth).toFixed(2);
+        document.getElementById("circle2").style.transform = "scale(" + 1 / x + ")"
+
+        $("#circle").height(250 / x)
+        $("#circle").width(250 / x)
+        $("#circle").css({marginTop: -125 / x, marginLeft: -125 / x})
+    }
+
     $("body").mousemove(function (e) {
         handleMouseMove(e);
     });
+
+    function handleMouseMove(event) {
+
+        var x = event.pageX < ($(window).width() - 125 / (window.defaultWidth / window.innerWidth).toFixed(2)) ? event.pageX : ($(window).width() - 125 / (window.defaultWidth / window.innerWidth).toFixed(2));
+        var y = event.pageY < ($(window).height() - 125 / (window.defaultWidth / window.innerWidth).toFixed(2)) ? event.pageY : ($(window).height() - 125 / (window.defaultWidth / window.innerWidth).toFixed(2));
+
+        $("#circle").animate({
+            left: x,
+            top: y
+        }, 0);
+
+        $("#circle2").animate({
+            left: x,
+            top: y
+        }, 0);
+
+    }
 
     $("#button-interface").click(
         function (e) {
@@ -9,28 +37,17 @@ $(document).ready(function () {
         }
     );
 
-    function handleMouseMove(event) {
-        let x = event.pageX,
-            y = event.pageY;
-
-        $("#circle").animate({
-            left: x,
-            top: y
-        }, 0);
-    }
-    
     function interfaceMoveOut(event) {
         let interfase = $("#interface"),
             flag = interfase.css("display") == "none" ? false : true;
-        if(flag){
+        if (flag) {
             interfase.hide(1000);
             interfase.empty();
             $("#button-interface").animate({
                 left: "6%",
                 top: "2%"
             })
-        }
-        else{
+        } else {
             interfase.show("slow");
             $("#button-interface").animate({
                 left: "1%",
@@ -39,4 +56,5 @@ $(document).ready(function () {
         }
     }
 });
+
 
