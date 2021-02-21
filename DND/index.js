f4be4d3307bf6aa050fb624243ff305b2b54cf4c
svyat935 <svyat935@mail.ru>
2021-02-21 16:27:04 +0300

    Added interface(raw) and separated style, scripts and html.

---
+++ DND/index.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $("body").mousemove(function (e) {
+        handleMouseMove(e);
+    });
+
+    $("#button-interface").click(
+        function (e) {
+            interfaceMoveOut(e);
+        }
+    );
+
+    function handleMouseMove(event) {
+        let x = event.pageX,
+            y = event.pageY;
+
+        $("#circle").animate({
+            left: x,
+            top: y
+        }, 0);
+    }
+    
+    function interfaceMoveOut(event) {
+        let interfase = $("#interface"),
+            flag = interfase.css("display") == "none" ? false : true;
+        if(flag)
+            interfase.hide(1000);
+        else
+            interfase.show("slow");
+    }
+});
+
